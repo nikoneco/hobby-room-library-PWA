d511c17e1c4db21500c225156a728a0afee270f3
NikoNeco <aqua_aqua@live.jp>
2026-07-07 19:24:15 +0900

    Refine shelf room map surface

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "shumi-library-pwa-da8a3bacdf97";
+const CACHE_NAME = "shumi-library-pwa-7f7e6acda2a0";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -21,7 +21,7 @@ const APP_SHELL = [
   "./assets/js/script.images.b60e3bc29c.js",
   "./assets/js/script.search.7697a1785a.js",
   "./assets/js/script.render.921eff7a34.js",
-  "./assets/js/script.shelf.81416c50e2.js",
+  "./assets/js/script.shelf.598dbdb6f7.js",
   "./assets/js/script.modal.832c6724b5.js",
   "./assets/js/script.boot.517c07a9bf.js",
   "./assets/icons/icon-lantern-192.png",
